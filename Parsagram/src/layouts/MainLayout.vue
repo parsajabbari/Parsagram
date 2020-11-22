e9c18b9ff3dd3f49c6ba3a31f5815abe83356730
Parsa Jabbari <parsajabbari@gmail.com>
2020-11-21 20:51:37 -0800

    setup top/bottom navigation bars

--- Parsagram/src/layouts/MainLayout.vue
+++ Parsagram/src/layouts/MainLayout.vue
@@ -1,25 +1,18 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
-          Quasar App
+    <q-header class="bg-white text-grey-10" bordered>
+      <q-toolbar class="constrain">
+        <q-btn flat round dense to="/camera" size="18px" icon="eva-camera-outline" class="large-screen-only q-mr-sm" />
+        <q-separator vertical spaced class="large-screen-only" />
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          Parsagram
         </q-toolbar-title>
+        <q-btn flat round dense to="/" size="18px" icon="eva-home-outline" class="large-screen-only" />
 
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-footer bordered class="bg-white">
+    <q-footer bordered class="bg-white small-screen-only">
       <q-tabs class="text-grey-10"
         active-color="primary"
         indicator-color="transparent">
@@ -47,6 +40,13 @@ export default {
 </script>
 
 <style lang="sass">
+  .q-toolbar
+    @media (min-width: $breakpoint-sm-min)
+      height: 77px
+  .q-toolbar__title
+    @media (max-width: $breakpoint-xs-max)
+      text-align: center
+    font-size: 30px
   .q-footer 
     .q-tab__icon 
       font-size: 30px
