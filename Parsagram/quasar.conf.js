f617cb7b719d6178383a4cb917c46ad7bd9934fd
Parsa Jabbari <parsajabbari@gmail.com>
2020-11-23 09:49:40 -0800

    first working version holy crap

--- Parsagram/quasar.conf.js
+++ Parsagram/quasar.conf.js
@@ -48,7 +48,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       env: {
-        API: API_LOCAL
+        API: API_PRODUCTION
         // "https://parsagram-backend.herokuapp.com" or "http://localhost:3000"
       },
       vueRouterMode: 'history', // available values: 'hash', 'history'
