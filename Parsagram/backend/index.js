0cc0f8142d3977f7b8d978851336a76be433c597
Parsa Jabbari <parsajabbari@gmail.com>
2020-11-23 00:44:16 -0800

    finish 91

--- Parsagram/backend/index.js
+++ Parsagram/backend/index.js
@@ -3,17 +3,23 @@ const express = require('express')
 const admin = require('firebase-admin');
 let inspect = require('util').inspect;
 let Busboy = require('busboy');
-
+let path = require('path')
+let os = require('os')
+let fs = require('fs')
+const { v4: UUID } = require('uuid');
+//let UUID = require('uuid-v4')
 
 
 // Config Firebase
 const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "project-parsagram.appspot.com"
 });
 
 const db = admin.firestore();
+let bucket = admin.storage().bucket();
 
 // Config Express
 const app = express()
@@ -35,33 +41,61 @@ app.get('/posts', (request, response) => {
 
 // Endpoint - Create Post
 app.post('/createPost', (request, response) => {
-    response.set("Access-Control-Allow-Origin", "*") 
-    var busboy = new Busboy({ headers: request.headers });
-    let fields = {}
-    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-      console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-      file.on('data', function(data) {
-        console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
-      });
-      file.on('end', function() {
-        console.log('File [' + fieldname + '] Finished');
-      });
-    });
-    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-      fields[fieldname] = val
-    });
-    busboy.on('finish', function() {
-      db.collection("posts").doc(fields.id).set({
+  response.set('Access-Control-Allow-Origin', '*')
+
+  let uuid = UUID()
+
+  var busboy = new Busboy({ headers: request.headers });
+
+  let fields = {}
+  let fileData = {}
+
+  busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+    console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+    // /tmp/4564564-234234.png
+    let filepath = path.join(os.tmpdir(), filename)
+    file.pipe(fs.createWriteStream(filepath))
+    fileData = { filepath, mimetype }
+  });
+
+  busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+    fields[fieldname] = val
+  });
+
+  busboy.on('finish', function() {
+    
+    bucket.upload(
+      fileData.filepath,
+      {
+        uploadType: 'media',
+        metadata: {
+          metadata: {
+            contentType: fileData.mimetype,
+            firebaseStorageDownloadTokens: uuid
+          }
+        }
+      },
+      (err, uploadedFile) => {
+        if (!err) {
+          createDocument(uploadedFile)
+        }
+      }
+    )
+
+    function createDocument(uploadedFile) {
+      db.collection('posts').doc(fields.id).set({
         id: fields.id,
         caption: fields.caption,
         location: fields.location,
         date: parseInt(fields.date),
-        imageUrl: "https://firebasestorage.googleapis.com/v0/b/project-parsagram.appspot.com/o/5tUZBEy.jpg?alt=media&token=306b6365-e100-4615-8027-f10e4d577831"
-
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${ bucket.name }/o/${ uploadedFile.name }?alt=media&token=${ uuid }`
+      }).then(() => {
+        response.send('Post added: ' + fields.id)
       })
-      response.send("done!");
-    });
-    request.pipe(busboy);
+    }
+  });
+
+  request.pipe(busboy)
 })
 
 // Listen
