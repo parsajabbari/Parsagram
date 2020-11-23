a323ed0bfbf5b7c97b2024ecedc9b36cd25fa875
Parsa Jabbari <parsajabbari@gmail.com>
2020-11-22 19:48:43 -0800

    link post endpoint to firebase

--- Parsagram/backend/index.js
+++ Parsagram/backend/index.js
@@ -1,26 +1,35 @@
 // Dependencies
 const express = require('express')
+const admin = require('firebase-admin');
 
-// Config
+
+// Config Firebase
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+// Config Express
 const app = express()
 const port = 3000
 
 // Endpoint - Posts
 app.get('/posts', (request, response) => {
-    let posts = [
-       {
-           caption: "Golden Gate Bridge",
-           location: "San Francisco"
-       }, 
-       {
-           caption: "London Eye",
-           location: "London"
-       }
-    ]
+    let posts = []
+    db.collection('posts').get().then(snapshot =>  {
+        snapshot.forEach((doc) => {
+        posts.push(doc.data())
+    });
     response.send(posts)
+    
+});
+    
 })
 
 // Listen
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Parsagram listening at http://localhost:${port}`)
 })
