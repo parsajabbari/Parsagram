9a951e1479cf4dcd135b0b43fb7c89b118c617ca
Parsa Jabbari <parsajabbari@gmail.com>
2020-11-22 23:07:55 -0800

    87

--- Parsagram/backend/index.js
+++ Parsagram/backend/index.js
@@ -37,6 +37,7 @@ app.get('/posts', (request, response) => {
 app.post('/createPost', (request, response) => {
     response.set("Access-Control-Allow-Origin", "*") 
     var busboy = new Busboy({ headers: request.headers });
+    let fields = {}
     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
       console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
       file.on('data', function(data) {
@@ -47,11 +48,17 @@ app.post('/createPost', (request, response) => {
       });
     });
     busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-      console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+      fields[fieldname] = val
     });
     busboy.on('finish', function() {
-      console.log('Done parsing form!');
-      //response.writeHead(303, { Connection: 'close', Location: '/' });
+      db.collection("posts").doc(fields.id).set({
+        id: fields.id,
+        caption: fields.caption,
+        location: fields.location,
+        date: fields.date,
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/project-parsagram.appspot.com/o/5tUZBEy.jpg?alt=media&token=306b6365-e100-4615-8027-f10e4d577831"
+
+      })
       response.send("done!");
     });
     request.pipe(busboy);
